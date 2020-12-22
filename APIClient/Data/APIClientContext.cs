0ce333f8e8977b57614003b8eabbb6ebf4a8b89b
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-12-22 20:40:35 -0300

    finalization from api initial

---
+++ APIClient/Data/APIClientContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIClient.Models;
+
+namespace APIClient.Data
+{
+    public class APIClientContext : DbContext
+    {
+        public APIClientContext (DbContextOptions<APIClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIClient.Models.Client> Client { get; set; }
+    }
+}
---
+++ APIClient/Models/Client.cs
@@ -0,0 +1,39 @@
+﻿//********************************************************************
+// Criação do arquivo Client
+//********************************************************************
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIClient.Models
+{
+    /// <summary>
+    /// classe representativa da tabela Client
+    /// </summary>
+    [Table("Client")]
+    public class Client
+    {
+        /// <summary>
+        /// ID do cliente
+        /// </summary>
+        [Key]
+        public int ClientID { get; set; }
+
+        /// <summary>
+        /// Nome do cliente
+        /// </summary>
+        [MaxLength(50,ErrorMessage = "O campo Nome suporta apenas {1} caracteres")]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Email do cliente
+        /// </summary>
+        [MaxLength(100,ErrorMessage = "O campo Email suporta apenas {1} caracteres")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Situação do cliente
+        /// </summary>
+        public bool Ativo { get; set; }
+    }
+}
